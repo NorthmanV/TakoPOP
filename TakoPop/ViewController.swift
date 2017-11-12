de96a934bbd30b16bedd643b25f88219c5b3d09d
Ruslan Akberov <ruslanakberov@gmail.com>
2017-11-12 23:03:16 +0300

    Initial Commit

---
+++ TakoPop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TakoPop
+//
+//  Created by Руслан Акберов on 12.11.2017.
+//  Copyright © 2017 Ruslan Akberov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
